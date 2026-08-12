336751e2ef82e0b32e1ac234c60ce3823a9b035e
Ajaykumar <ajaykumarbalusu77@gmail.com>
2026-08-12 17:47:08 +0530

    terrsform code for configuring the S3_DynamoDB

---
+++ Terraform_DynamoDB_setup/main.tf
@@ -0,0 +1,12 @@
+terraform { 
+backend "s3" { 
+bucket         
+key            
+region         
+= "company-tfstate" 
+= "prod/network/terraform.tfstate" 
+= "ap-south-1" 
+dynamodb_table = "tf-lock" 
+encrypt        
+} 
+}
